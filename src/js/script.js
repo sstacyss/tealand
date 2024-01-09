c42f8031f43c8cf78cf6db9598d92f3c21f470fa
Stacy <anastasiass96@gmail.com>
2024-01-09 14:23:15 +0200

    initial_commit

---
+++ src/js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+  // Общие настройки для всех слайдеров
+  const commonSettings = {
+    infinite: true,
+    autoplay: true,
+    arrows: true,
+  };
+
+  // Инициализация первого слайдера
+  $(".slick-carousel").slick(
+    $.extend({}, commonSettings, {
+      prevArrow:
+        '<button type="button" class="slick-prev"><img src="icons/arrow_previous.svg"></button>',
+      nextArrow:
+        '<button type="button" class="slick-next"><img src="icons/arrow_next.svg"></button>',
+      speed: 3500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplaySpeed: 3500,
+      cssEase: "linear",
+    })
+  );
+
+  // Инициализация второго слайдера
+  $(".multiple-carousel").slick(
+    $.extend({}, commonSettings, {
+      speed: 8000,
+      slidesToShow: 6,
+      slidesToScroll: 2,
+      autoplaySpeed: 0,
+      cssEase: "linear",
+    })
+  );
+});
+
+// $(document).ready(function () {
+//   $(".slick-carousel").slick({
+//     prevArrow:
+//       '<button type="button" class="slick-prev"><img src="icons/arrow_previous.svg"></button>',
+//     nextArrow:
+//       '<button type="button" class="slick-next"><img src="icons/arrow_next.svg"></button>',
+//     infinite: true,
+//     autoplay: true,
+//     speed: 500,
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     arrows: true,
+//     autoplaySpeed: 2500,
+//   });
+// });
+
+// $(document).ready(function () {
+//   $(".multiple-carousel").slick({
+//     infinite: true,
+//     autoplay: true,
+//     speed: 8000,
+//     slidesToShow: 6,
+//     slidesToScroll: 2,
+//     autoplaySpeed: 0,
+//     cssEase: "linear",
+//   });
+// });
